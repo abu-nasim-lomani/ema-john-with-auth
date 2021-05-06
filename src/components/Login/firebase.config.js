d012577197ecfadfcf0b6b1938634c1437863f45
Abu Nasim Lomani <info.abunasim@gmail.com>
2021-05-06 23:26:39 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAKcAha1gjsANJrOgjs_bTHs9wgWr5LgT4",
+  authDomain: "ema-jhon-simple-project-47c29.firebaseapp.com",
+  projectId: "ema-jhon-simple-project-47c29",
+  storageBucket: "ema-jhon-simple-project-47c29.appspot.com",
+  messagingSenderId: "153136142375",
+  appId: "1:153136142375:web:c31619e20fcfc5eacb5602"
+};
+  export default firebaseConfig;
